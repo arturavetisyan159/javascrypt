e909f5d544d3bf2873a5af867c186f962f7c9f2f
artur <artur-avetisyan-90@inbox.ru>
2021-05-20 18:52:36 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// console.log("hello");
+
+//const pi = 3.14; // создаю константу pi (const- ключевое слово)
+//R = 3390;
+//const S = 4 * pi * R * R;
+
+//console.log(S)
+
+//const weight = 65;
+//const c = 300000000;
+//const E = weight * c * c;
+//есть константы, а есть также переменные- "let", которые в отличие от констант можно изменять. 
+//console.log(E);
+// есть такое понятие как арность- это количество аргуметов, например + - * это бинарные операторы, так как им нужно 2 операнда, следовательно их арность = 2.
+
+//const pi = 3.14;
+//const surfaceOfMars = 4 * pi * 3390 * 3390;
+//const surfaceOfMercury = 4 * pi * 2440 * 2440;
+
+//const surfaceOfMercury = surfaceAreaCalculator(2440);
+
+//const surfaceAreaCalculator = (radius) => {
+    //return 4 * pi * radius * radius;
+//};
+//const surfaceOfMars = surfaceAreaCalculator(3390);
+//const surfaceOfMercury = surfaceAreaCalculator(2440);
+//console.log(surfaceOfMars);
+//console.log(surfaceOfMercury);
+
+// const percentageCalculator = (number, total) => {
+//     return number * 100 / total
+// };
+
+// console.log(percentageCalculator(40, 80))
+
+// const squareOfSum = (x, y) => (x + y) ** 2;
+// const firstNum = 2;
+// const secondNum = 3; 
+// console.log(squareOfSum(firstNum, secondNum));
+
+// формальные и фактические параметры функций
+// формальные - имена переменных в определении функции (a, b).
+// фактические - это те, которые были переданы в функцию в момент вызова (5, 3).
