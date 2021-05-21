f0604c589f8e299feb72c831d865a705aeb0e86d
artur <artur-avetisyan-90@inbox.ru>
2021-05-21 21:43:18 +0300

    COMMIT

--- js/operator.js
+++ js/operator.js
@@ -1,8 +1,13 @@
 // тернарный оператор, это сокращенный опреатор  if.
 // сохраняем результа вычисления в константе
-const num = -50
-const absValue = (num === 0 || num > 0) ? num : -num; // тернарная операция выисляет и возвращает значение, то етсь является выражением.
-console.log(absValue);
-// можно результат вычисления тернарной операции вернуть из функции
-const getAbs = num => (num >= 0) ? num : -num;
-console.log(getAbs);
+// const num = -50
+// const absValue = (num === 0 || num > 0) ? num : -num; // тернарная операция выисляет и возвращает значение, то етсь является выражением.
+// console.log(absValue);
+// // можно результат вычисления тернарной операции вернуть из функции
+// const getAbs = num => (num >= 0) ? num : -num;
+// console.log(getAbs);
+
+
+// const color = "white";
+// const getColor = color => (color === 'white') ? color : 'black';
+// console.log(getColor(color));
