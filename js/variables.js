c4786323275e83267e63b2065826f19e7f090326
artur <artur-avetisyan-90@inbox.ru>
2021-05-26 22:44:44 +0300

    class-6 end of 3 lection

--- js/variables.js
+++ js/variables.js
@@ -2,13 +2,22 @@
 // let и var по разному влияют на видимость переменной. Советуют использовать let, при обьявлении переменной, а не var.
 const let1 = "they call him \"Tommy\" and he likes it "; // \..\ - это экраннирование, шоб не было ошибок.
 // если нам в строке нужен обратный слэш , нам нужно его тоже экранировать.
-const skip = str => {
-    let i = 0;
-    let result = '';
-    while (i < str.length){
-        result = result + str[i];
-        i = i + 2;
-    }
-    return result;
-};
-console.log(skip('California'));
+// в строках "/t"- это таб, "/n"- это перенос строки.
+
+
+// const skip = str => {
+//     let i = 0;
+//     let result = '';
+//     while (i < str.length){
+//         result = result + str[i];
+//         i = i + 2;
+//     }
+//     return result;
+// };
+// console.log(skip('California'));
+
+
+const name = "Alex";
+const a = 5;
+const b = 6;
+console.log(`his name is ${name} and he is ${a+b} years old`); // вот такие ковычки испрльзуем (обратные), также используем знак $
