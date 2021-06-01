e8215e4df3261b10b98ba9b9bb3e72b594755d10
artur <artur-avetisyan-90@inbox.ru>
2021-06-01 20:10:53 +0300

    class-1_dnaToRna

--- js/scope.js
+++ js/scope.js
@@ -11,14 +11,56 @@
 
 // let result = true; // глобальная область видимости\
 
-const createPrinter = () => {
-    const name = 'King';
+// const createPrinter = () => {
+//     const name = 'King';
 
-    const printName = () => {
-        console.log(name);
+//     const printName = () => {
+//         console.log(name);
+//     }
+//     return printName;
+// }
+//  const myPrinter = createPrinter();
+//  myPrinter();
+
+// const x = 7;
+// const f = (x) => () => () => x;
+// console.log(f(10)()()); // 10
+
+// const isRightNum = num => {
+//     const res = (num > 3) && (num ** 2 < 26) && (num != 5);
+//     return res;
+// };
+
+// console.log(isRightNum(4)); // true
+
+// const value = 777;
+// const func = value => value + 1;
+// console.log(func(5)); // 6
+
+// Преобразование ДНК в РНК. Задача
+// Четыре нуклеотид в ДНК- это аденин (A), цитозин (C), гуанин (G), тимин(T);
+// Четыре нуклеотид в РНК- это аденин (A), цитозин (C), гуанин (G), урацил(T);
+
+const dnaToRna = dna => {
+    if (dna.length === 0){
+        return '';
     }
-    return printName;
+    let rna = '';
+    for (let i = 0; i < dna.length; i++){
+        if ((dna[i] != 'A') && (dna[i] != 'G') && (dna[i] != 'C') && (dna[i] != 'T')){
+            rna = null;
+            break;
+        } else if (dna[i] === 'A') {
+            rna = rna + 'U';
+        } else if (dna[i] === 'G') {
+            rna = rna + 'C';
+        } else if (dna[i] === 'T') {
+            rna = rna + 'A';
+        } else if (dna[i] === 'C') {
+            rna = rna + 'G';
+        }
+    }
+    return rna;
 }
- const myPrinter = createPrinter();
- myPrinter();
- 
+
+console.log('Получившееся РНК= ' + dnaToRna('ADCTGU'));
