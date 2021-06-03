eded71d18ae4b7380e58b57d4974535cc4fc82b0
artur <artur-avetisyan-90@inbox.ru>
2021-06-03 21:08:21 +0300

    class-1 закончил лекцию массивы

--- js/arrays.js
+++ js/arrays.js
@@ -63,17 +63,124 @@
 
 // ЗАДАЧА. функция которая меняет местами 1 и последний эл массива. Если массив содержит <2 элементов, то возвращается как есть!
 
-const changeElements = n => {
-    if(n.length == 1 ){
-        return n;
-    }else{
-        const first = n[0];
-        const last = n[n.length - 1];
-        n[0] = last;
-        n[n.length - 1] = first;
-        return n;
-    }
+// const changeElements = n => {
+//     if(n.length == 1 ){
+//         return n;
+//     }else{
+//         const first = n[0];
+//         const last = n[n.length - 1];
+//         n[0] = last;
+//         n[n.length - 1] = first;
+//         return n;
+//     }
     
-}
+// }
 
-console.log(changeElements([1, 2, 3, 4, 5]));
+// console.log(changeElements([1, 2, 3, 4, 5]));
+
+// const items = [2, 8, 1];
+// items.sort();
+// console.log(items);
+// items.reverse();
+// console.log(items);
+
+// const array1 = [1, 2];
+// const array2 = [3, 4];
+// const array3 = array1.concat(array2); // [1, 2, 3, 4]
+
+
+// ЗАДАЧА. Функция, которая принимает массив и строковый префикс и возвращает новый массив, в котором каждому элементу исходного массива добавляется префикс. функция предназначена для работы со строковыми элементами. для префикса автоматически добавляется пробел.
+// const names = ['John', 'Misha', 'Kolya'];
+
+// const addPrefix = (arr, prefix) => {
+//     let newArray = [];
+//     for(let i = 0; i < arr.length; i++){
+//         newArray.push(`${prefix} ${arr[i]}`)
+//     }
+//     return newArray;
+// }
+
+// console.log(addPrefix(names, 'Mr'));
+
+// ЗАДАЧА. Функция высчитывает сумму всех элементов массива, которые делятся без остатка на 3.
+// const numbers = [1, 2, 4, 3, 5, 6, 9, 12];
+
+// const isDivThree = arr => {
+//     let sum = 0;
+//     if(arr.length == 0){
+//         return 0;
+//     }else {
+//         for(let i = 0; i < arr.length; i++){
+//             if(arr[i] % 3 === 0){
+//                 sum += arr[i];
+//             }
+//         }
+//     }
+//     return sum;    
+// }
+// console.log(isDivThree(numbers));
+
+
+// for ... of
+// const names = ['John', 'Misha', 'Kolya'];
+// // name на каждой итерации свой собственный(локальный), поэтому используется const
+// for(const name of names){
+//     console.log(name);
+// }
+
+// можем строку посимвольно разобрать.
+// const greeting = 'Hello';
+
+// for(const symbol of greeting){
+//     console.log(symbol);
+// }
+
+// УДАЛЕНИЕ ЭЛЕМЕНТА МАССИВА. delete - нежелательная операция. 
+// Есть метод  .splice()
+// Можно описать функцию удаления элемнта из массива
+// const data = [0, 1, false, true, null, 'wow'];
+// const compact = coll => {
+//     const result = [];
+//     for(const item of coll){
+//         if(item != null){
+//             result.push(item);
+//         }
+//     }
+//     return result;
+// }
+// const newArray = compact(data);
+// console.log(newArray);
+
+// операторы break и continue.
+
+// ГЕНЕРАЦИЯ СТРОК В ЦИКЛЕ (не оч удобно)
+// const coll = ['milk', 'butter'];
+
+// const buildHTMLList = coll => {
+//     let result = '<ul>';
+//     for (const item of coll){
+//         result = `${result}<li>${item}</li>`;
+//         result = `${result}</ul>`
+//     }
+//     return result;
+// }
+// console.log(buildHTMLList(coll));
+
+// ГЕНЕРАЦИЯ СТРОК МЕТОДОМ .JOIN() - этот метод объединяет элементы массива в строку
+
+// const coll = ['milk', 'butter'];
+
+// const buildHTMLList = coll => {
+//     const parts = [];
+//     for(const item of coll){
+//         parts.push(`<li>${item}</li>`)
+//     }
+//     const innerValue = parts.join('');
+//     const result = `<ul>${innerValue}</ul>`;
+//     return result; 
+// }
+// console.log(buildHTMLList(coll));
+
+// const parts = ['JS', 'Python', 'Parts'];
+// const output = parts.join(', '); // JS, Python, Parts
+// console.log(output);
