92358386632ec93230b14fa19de75bfa944ee26d
artur <artur-avetisyan-90@inbox.ru>
2021-06-03 17:21:21 +0300

    class-1 arrays_task1

--- js/arrays.js
+++ js/arrays.js
@@ -25,6 +25,55 @@
 // animals.unshift('horses'); // Добавляет В НАЧАЛО элемент 'horses'
 
 // УДАЛЕНИЕ
-const animals = ['cats', 'dogs', 'birds'];
-delete animals[1]; // удалление элемента  с индексом 1. при таком удалении размер массива не меняется
-console.log(animals); //  ['cats', 'empty item', 'birds']; и длина будет равна 3 все равно
+// const animals = ['cats', 'dogs', 'birds'];
+// delete animals[1]; // удалление элемента  с индексом 1. при таком удалении размер массива не меняется
+// console.log(animals); //  ['cats', 'empty item', 'birds']; и длина будет равна 3 все равно
+
+
+// цикл for при работе с массивом
+// перебираем все элемнты 
+// const names = ['vasya', 'vanya', 'petya'];
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+// изменение
+// const emails = ['VASYA@gmAil.com', 'IGOR@gmail.com', 'netiD@hOt.CoM'];
+// console.log(emails);
+// let res = [];
+// for(let i = 0; i < emails.length; i +=1){
+//     const email = emails[i];
+//     const normalized = email.toLowerCase();
+//     res.push(normalized);
+// }
+// console.log(res);
+
+// Ссылки. let и const в js хранят два типа данных: примитивные и ссылочные
+// 1) числа, строки, булеан и тд
+// 2) объекты
+
+// const items = [1,2];
+// const items2 = items;
+// items.push(3);
+
+// items2 === items // true (потому что ссылка на один и тот же объект)
+
+// console.log(items);
+// console.log(items2);
+
+// ЗАДАЧА. функция которая меняет местами 1 и последний эл массива. Если массив содержит <2 элементов, то возвращается как есть!
+
+const changeElements = n => {
+    if(n.length == 1 ){
+        return n;
+    }else{
+        const first = n[0];
+        const last = n[n.length - 1];
+        n[0] = last;
+        n[n.length - 1] = first;
+        return n;
+    }
+    
+}
+
+console.log(changeElements([1, 2, 3, 4, 5]));
