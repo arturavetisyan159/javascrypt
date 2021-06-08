c3ad7512ed525c2b86eab3f4eedfa55589152064
artur <artur-avetisyan-90@inbox.ru>
2021-06-08 13:13:52 +0300

    class-1 rest opreator

--- js/arrays_2.js
+++ js/arrays_2.js
@@ -43,3 +43,54 @@
 // console.log(element2);
 
 // console.log(stack);
+
+// деструктуризация 
+// const points = [3, 5];
+// console.log(`${points[0]}:${points[1]}`);
+
+// const x = points[0];
+// const y = points[1];
+// console.log(`${x}:${y}`);
+
+// const [x,y] = points; // [x, y] = [3, 5]
+// console.log(`${x}:${y}`);
+
+// const points = [3, 5];
+// const [x] = points; // 3
+// console.log(x);
+
+// const [, y]= points;
+// console.log(y); // 5
+
+// const [first, second, third] = 'two';
+// console.log(first); // t
+// console.log(second); // w
+// console.log(third); // o
+
+// const points = [
+//     [4,3],
+//     [0,-3]
+// ];
+// for (const [x, y] of points){
+//     console.log([x, y]) // [4, 3] [0, -3]
+// }
+
+// rest-оператор 
+
+// const fruits = ['apple', 'pineapple', 'banana', 'orange'];
+// const [first, ...rest] = fruits; // сворачивает часть элементов и помещает их в массив с именем rest(или с именем какое вы захотите). rest-опреатор срабатывает в конце.
+// console.log(first);
+// console.log(rest);
+
+// когда интересна только часть массива, и не важны первые элементы, используем метод slice()
+
+// const fruits = ['apple', 'pineapple', 'banana', 'orange'];
+// const rest = fruits.slice(1); // ['pineapple', 'banana', 'orange']
+// console.log(rest);
+// console.log(fruits);
+
+// rest-оператор при деструктуризации строк
+const [first, second, ...rest] = 'some string';
+console.log(first); // 's'
+console.log(second); // 'o'
+console.log(rest); // после применения rest мы записали в rest массив строк
